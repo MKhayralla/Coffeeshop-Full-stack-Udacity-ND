81046fe4a2013533ebb8ab4357341abe1bf459ed
Mkhayralla <gonfreecs303@gmail.com>
2021-09-09 10:41:37 +0200

    edited .gitignore

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,9 +4,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-vy99-vnq.us', // the auth0 domain prefix
-    audience: 'https://drinks.udacity.api', // the audience set for the auth0 app
-    clientId: 'ms4UMzsXud6Dc6Qc1ihmRhNSwbOsY9Q5', // the client id generated for the auth0 app
+    url: 'your auth0 tenant', // the auth0 domain prefix
+    audience: 'your API identifier', // the audience set for the auth0 app
+    clientId: 'your application clientID', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:3000', // the base url of the running ionic application. 
   }
 };
